c1779bfe6c5a57da5a25cd31e0ee09d3ad482843
DiarafaSoukouna <diarafasouk@gmail.com>
2024-02-22 14:54:26 +0000

    First commit

---
+++ src/components/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <BContainer fluid>
+      <BRow>
+        <BCol col sm="6">
+          {{ new Date().getFullYear() }} © Copyright | Tous droits reservés
+        </BCol>
+        <BCol col sm="6">
+          <div class="text-sm-end d-none d-sm-block">COSIT</div>
+        </BCol>
+      </BRow>
+    </BContainer>
+  </footer>
+</template>
